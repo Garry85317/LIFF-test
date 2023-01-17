be3d68e702a23386639105cf7f475da3ace53b44
garry85317@gmail.com <garry85317@gmail.com>
2023-01-17 21:56:36 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID,
+      })
+      .then(() => {
+        setMessage("LIFF init succeeded.");
+        const idToken = liff.getDecodedIDToken();
+        console.log(idToken);
+      })
+      .catch((e) => {
+        setMessage("LIFF init failed.");
+        setError(`${e}`);
+      });
+  });
+
+  return (
+    <div className="App">
+      <h1>create-liff-app</h1>
+      {message && <p>{message}</p>}
+      {error && (
+        <p>
+          <code>{error}</code>
+        </p>
+      )}
+      <a
+        href="https://developers.line.biz/ja/docs/liff/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LIFF Documentation
+      </a>
+    </div>
+  );
+}
+
+export default App;
